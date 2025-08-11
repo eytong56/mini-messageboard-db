09c8f22bbe71fcc8e5c202708e77769b16d4d47e
eytong56 <emmatong56@gmail.com>
2025-08-11 14:43:43 -0700

    Change __dirname to traditional approach

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import "dotenv/config";
 import router from "./routers/router.js";
-const __dirname = import.meta.dirname;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 
 const app = express();
